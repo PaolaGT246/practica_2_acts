02cb5cb7d8e0807a4bff8b736b186a747f42d308
PaolaGT246 <a.23308051281187@cbtis128.edu.mx>
2024-10-10 23:09:33 -0600

    Add files via upload

---
+++ saludo.py
@@ -0,0 +1,9 @@
+#Funcion que muestre saludo Hey amigos! cada vez que se le pida
+print(" ")
+print("Gutierrez Torres Paola")
+print(" ")
+def saludar():
+    print("Hey amigos!")
+
+# Llamar a la función
+saludar()
